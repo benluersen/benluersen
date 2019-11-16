6493f7f887e41a5376715af95de323e6cc78c83b
ben.luersen@gmail.com <ben.luersen@gmail.com>
2019-11-16 11:48:28 -0500

    538 dice challenge completed

---
+++ 538 10-sided die/538 10-sided die/BusinessLogic.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _538_10_sided_die
+{
+    public class BusinessLogic
+    {
+        public double RollGame(int numPlays)
+        {
+            Random rnd = new Random();
+            double finalAvg = 0.0;
+            for (int i = 0; i < numPlays; i++)
+            {
+                List<int> rollList = new List<int>();
+                int counter = 0;
+                string rollResult = "0.";
+                bool exit = false;
+                int roll = rnd.Next(0, 10);
+                rollList.Add(roll);
+                while (!exit)
+                {
+                    roll = rnd.Next(0, 10);
+                    if (roll == 0)
+                    {
+                        exit = true;
+                    }
+                    else if (roll < rollList[counter])
+                    {
+                        rollList.Add(roll);
+                        counter++;
+                    }
+                }
+                foreach (var thing in rollList)
+                {
+                    rollResult += $"{thing}";
+                }
+                double rollResultdouble = Convert.ToDouble(rollResult);
+                finalAvg += rollResultdouble;
+            }
+            finalAvg = finalAvg / (numPlays);
+            return finalAvg;
+        }
+    }
+}
---
+++ 538 10-sided die/538 10-sided die/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using _538_10_sided_die;
+
+namespace _538_10_sided_die
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the number of times you'd like to simulate the game");
+            int numPlays = Convert.ToInt32(Console.ReadLine());
+            BusinessLogic bl = new BusinessLogic();
+            Console.WriteLine(bl.RollGame(numPlays));
+            Console.ReadKey();
+        }
+    }
+}
+
+
