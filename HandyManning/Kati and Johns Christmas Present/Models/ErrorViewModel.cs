1ea4b6440d72b377e7843f17296585cac1973ca9
Ben Luersen <ben.luersen@gmail.com>
2020-03-15 16:20:47 -0400

    update

---
+++ HandyManning/Kati and Johns Christmas Present/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Kati_and_Johns_Christmas_Present.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
